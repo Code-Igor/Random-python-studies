d57fe134d59a0c6279ce03a12890090a3dbccd39
Igor <igordossantoslopes73@gmail.com>
2024-08-12 21:39:56 -0300

    for 1
    .

---
+++ for, testing.py
@@ -0,0 +1,23 @@
+#x = float(input(""))
+
+#total += x ## O += vai acrescentando e somando os valores da variável x na variável total
+
+#for i in range(10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000, -1, -10000000000000000000000000000000000000000000000000000):
+  #print(i)
+
+for i in range(1, 5):
+  print(i)
+
+for i in range(1, 10, 2): #1 é o inicio, 10 o fim e 2 o passo.
+  print(i)
+
+for i in range(10, 0, -1):
+  print(i)
+
+
+a = float(input("Informe um valor: "))
+
+for i in range (50):
+  if a == 10:
+    print(10)
+
